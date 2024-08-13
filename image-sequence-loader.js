69292a50b9822d520fe0e424a51ab5d5a9c3da9a
Cameron Lewis <cameronwlewis@users.noreply.github.com>
2024-08-13 14:47:12 -0700

    Add files via upload

--- image-sequence-loader.js
+++ image-sequence-loader.js
@@ -1,15 +1,11 @@
-// script.js
+let cloudImage360 = document.querySelector('.cloudimage-360');
+const imagesData = cloudImage360.getAttribute('data-folder');
+const images = JSON.parse(imagesData);
 
-function imageSequenceLoader(){
-    let cloudImage360 = document.querySelector('.cloudimage-360');
-    const imagesData = cloudImage360.getAttribute('data-folder');
-    const images = JSON.parse(imagesData);
-    
-    // Create image elements and append them to the body
-    let imageElements = []
-    images.forEach(base64 => {
-        imageElements.push('data:image/jpeg;base64,' + base64);
-    });
-    imageElements = JSON.stringify(imageElements)
-    cloudImage360.setAttribute('data-folder', imageElements);
-}
+// Create image elements and append them to the body
+let imageElements = []
+images.forEach(base64 => {
+    imageElements.push('data:image/jpeg;base64,' + base64);
+});
+imageElements = JSON.stringify(imageElements)
+cloudImage360.setAttribute('data-folder', imageElements);
